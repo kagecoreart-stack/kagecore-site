953e05006bf635d15a3f2162ab1d3cd32e90330f
kagecoreart-stack <kagecoreart@gmail.com>
2026-05-02 02:51:37 +0300

    Create layout.tsx

---
+++ app/layout.tsx
@@ -0,0 +1,9 @@
+import './globals.css';
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang="en">
+      <head><title>KAGECORE</title></head>
+      <body>{children}</body>
+    </html>
+  );
+}
